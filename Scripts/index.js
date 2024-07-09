4706a6ec3934717314ddee96eaddf0445d09d789
Ashish-Online <workforashishpatel@gmail.com>
2024-07-09 23:28:35 +0530

    Update Quantity Number in the Cart icon

--- Scripts/index.js
+++ Scripts/index.js
@@ -10,14 +10,20 @@ products.forEach((product) => {
         <div class="rating"><span class="rating-star"><img src="Images/ratings/rating-${product.rating.stars * 10}.png" alt=""></span>
         <span class="rating-number pointer">${product.rating.count}</span></div>
         <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
-        <select name="cars" id="quantity-number">
+        <select name="cars" id="quantity-number" class="js-quantity-selector-${product.id}">
           <option value="1">1</option>
           <option value="2">2</option>
-          <option value="1">3</option>
+          <option value="3">3</option>
           <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
         </select>
         <div class="added-to-cart-message"></div>
-        <button type="button" data-product-id="${product.id}" class="add-to-cart-btn js-add-to-cart-btn">Add to Cart</button>
+        <button type="button" data-product-id="${product.id}" data-quantity-number="js-quantity-selector-${product.id}" class="add-to-cart-btn js-add-to-cart-btn">Add to Cart</button>
       </div>
   `;
 
@@ -26,12 +32,15 @@ gridContainer.innerHTML = productsHTML;
 
 const cartQuality = document.querySelector('.js-cart-quality');
 const addToCartBtns = document.querySelectorAll('.js-add-to-cart-btn');
-let totalCartQuality=0;
+let totalCartQuality = 0;
 
 function addToCart(button) {
   const productId = button.dataset.productId;
+  const selectQuantity = button.dataset.quantityNumber;
+  const quantityNumber = document.querySelector(`.${selectQuantity}`);  
+
   let matchingItem;
-  totalCartQuality=0;
+  totalCartQuality = 0;
 
   cart.forEach((item) => {
     if (productId === item.id) {
@@ -40,11 +49,11 @@ function addToCart(button) {
   });
 
   if (matchingItem) {
-    matchingItem.quantity += 1;
+    matchingItem.quantity += Number(quantityNumber.value);
   } else {
     cart.push({
       id: productId,
-      quantity: 1
+      quantity: Number(quantityNumber.value)
     });
   }
 
